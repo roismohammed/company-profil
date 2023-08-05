35fcc233048e4cd6a661b6aeb0286f7532689dbb
Mohammed Rois <id.roismohammed@gmail.com>
2023-08-06 06:51:50 +0700

    sd

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,6 +2,7 @@ import Guest from "./layouts/Guest";
 import './Index.css'
 import { RiArrowUpCircleFill } from "react-icons/ri";
 import { useState } from "react";
+import 'animate.css';
 export default function Index() {
     const [stikyIcon, setStikyIcon] = useState(false)
 
@@ -19,7 +20,7 @@ export default function Index() {
             <div className="img-container" id="atas">
                 <div className="container text-box d-flex align-items-center h-100 text-white ">
                     <div className="text-box">
-                        <h1 className="judul-index">Tusuk Sate,Murah</h1>
+                        <h1 className="judul-index animate__animated animate__fadeInDown">Tusuk Sate,Murah</h1>
                         <p >Melayani Jual Beli Tusuk Sate murah Dari Bambu harga terjangkau dan ekonomis dan lain-lain </p>
                         <div className="tombol   ">
                             <button className="btn btn-outline-light mx-2">Selengkapnya</button>
